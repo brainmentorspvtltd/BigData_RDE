004b09ca04a3bf79af0723d44fed907f8ad9dc64
Brain Mentors Pvt Ltd <brainmentorspvtltd@gmail.com>
2020-03-07 08:30:30 +0530

    Add files via upload

--- WordCount.java
+++ WordCount.java
@@ -1,40 +1,36 @@
 import java.io.IOException;
 import java.util.*;
 
+//Packages available in hadoop-common jar
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.conf.*;
 import org.apache.hadoop.io.*;
-
+//Packages available in hadoop-mapreduce-client-core jar
 import org.apache.hadoop.mapreduce.*;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
-
 public class WordCount {
-
-//	Mapper<KEYIN, VALUEIN, KEYOUT, VALUEOUT>
 	
-	public static class Map extends Mapper<LongWritable, Text,
-	Text, IntWritable>{
-			public void map(LongWritable key, Text value, Context context) 
-					throws IOException, InterruptedException {
-				String line = value.toString();
-				StringTokenizer token = new StringTokenizer(line);
-				while(token.hasMoreTokens()) {
-					value.set(token.nextToken());
-					context.write(value,  new IntWritable(1));
-				}				
+//	public static class Map extends Mapper<KEYIN, VALUEIN, KEYOUT, VALUEOUT>
+	public static class Map extends Mapper<LongWritable, Text, Text, IntWritable>{
+		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+			
+			String line = value.toString();
+			
+			StringTokenizer token = new StringTokenizer(line);
+			while(token.hasMoreTokens()) {
+				value.set(token.nextToken());
+				context.write(value, new IntWritable(1));
 			}
+		}
 	}
 	
-//	Reducer<KEYIN, VALUEIN, KEYOUT, VALUEOUT>
-	
-	public static class Reduce extends Reducer<Text, IntWritable,
-	Text, IntWritable> {
-		public void reduce(Text key, Iterable<IntWritable> values,
-				Context context) throws IOException, InterruptedException {
+//	public static class Reduce extends Reducer<KEYIN, VALUEIN, KEYOUT, VALUEOUT>
+	public static class Reduce extends Reducer<Text, IntWritable ,Text, IntWritable>{
+		public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
 			int sum = 0;
 			for(IntWritable x : values) {
 				sum += x.get();
@@ -42,27 +38,31 @@ public class WordCount {
 			context.write(key, new IntWritable(sum));
 		}
 	}
-	
-	
-	public static void main(String[] args) throws IOException, 
-	ClassNotFoundException, InterruptedException {
+
+	public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
 		Configuration conf = new Configuration();
-		Job job = Job.getInstance(conf, "WordCount");
+		
+		Job job = Job.getInstance(conf,"WordCount");
+		
 		job.setJarByClass(WordCount.class);
 		job.setMapperClass(Map.class);
-		job.setReducerClass(Reducer.class);		
+		job.setReducerClass(Reduce.class);
+		
 		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(IntWritable.class);		
+		job.setOutputValueClass(IntWritable.class);
+		
 		job.setInputFormatClass(TextInputFormat.class);
-		job.setOutputFormatClass(TextOutputFormat.class);		
+		job.setOutputFormatClass(TextOutputFormat.class);
+		
 		Path outputPath = new Path(args[1]);
+		
 		FileInputFormat.addInputPath(job, new Path(args[0]));
-		FileOutputFormat.setOutputPath(job, new Path(args[1]));		
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		
 		outputPath.getFileSystem(conf).delete(outputPath, true);
-		System.exit(job.waitForCompletion(true) ? 0 : 1);		
+		
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+		
 	}
 
 }
-
-
-
